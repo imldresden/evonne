b90f16dbfed118383f9cbc1e901d3cac59a24de0
Julián Méndez <menoc.sk27@gmail.com>
2025-04-16 16:20:50 +0200

    close linear mode curves so that clicking happens only on the link

--- frontend/public/js/proof/trees/linear.js
+++ frontend/public/js/proof/trees/linear.js
@@ -172,6 +172,9 @@ export class LinearNavigation {
         let offSetX = midpoint_x + ((this.bottomRoot ? 1 : -1) * offset * (dy / normalise));
         let offSetY = midpoint_y - offset * (dx / normalise);
 
-        return "M" + x2 + "," + y2 + "S" + offSetX + "," + offSetY + " " + x1 + "," + y1; // bezier curves
+        const forward = "M" + x2 + "," + y2 + "S" + offSetX + "," + offSetY + " " + x1 + "," + y1;
+        const backward = "M" + x2 + "," + y1 + "C" + offSetX + "," + offSetY + " " + x1 + "," + y2 + " " + x1 + "," + y2;
+        // close path on itself to avoid clickable block
+        return forward + backward; // bezier curves
     }
 }
