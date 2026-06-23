4f4ef1fa7b062ac8a1460e45c6dcd944662e442b
Julián Méndez <menoc.sk27@gmail.com>
2026-06-23 17:00:51 +0200

    clicks ignore foreign objects and line breaks ignore []

--- src/public/js/proof/node-visuals.js
+++ src/public/js/proof/node-visuals.js
@@ -260,15 +260,10 @@ export class NodeVisualsHelper {
             elements[i].append("foreignObject")
                 .attr("x", d => -(d.width) / 2 + TEXT_PAD)
                 .attr("y", d => -d.height + TEXT_PAD / 2)
+                .attr("pointer-events", "none") // foreign objects capture the clicks 
                 .attr("width", d => d.width)
                 .attr("height", d => d.height)
-                .html(d => `<div> ${this.getLabel(d.data.source)} </div>`);
-            // elements[i].append("text")
-            //     .attr("id", elementsID[i])
-            //     .attr("class", elementsClass[i])
-            //     .attr("x", d => (-d.width / 2) + (TEXT_PAD / (proof.isCompact ? 2 : 1)))
-            //     .attr("y", d => d.height / 1.5)
-            //     .text(d => this.getLabel(d.data.source)); 
+                .html(d => `${this.getLabel(d.data.source)}`);
         }
     }
 
@@ -353,8 +348,8 @@ export class NodeVisualsHelper {
             })
             .on("contextmenu", (e, d) => {
                 const menuItems = proof.axioms.menuItems;
-                e.preventDefault();
-                globals.contextMenu.create(e, d, menuItems.filter(m => m.filter && m.filter(d)), "#proof-view");
+                // e.preventDefault();
+                // globals.contextMenu.create(e, d, menuItems.filter(m => m.filter && m.filter(d)), "#proof-view");
             })
     }
 
@@ -673,7 +668,22 @@ export class NodeVisualsHelper {
         let ret = [];
         let lineCount = 0;
         let lastWasBreak = false;
+        let bypass = false;
         for (let char of label) {
+
+            if (char === "[") { // do not break until ]
+                bypass = true
+            }
+
+            if (bypass) {
+                ret.push(char);
+                if (char === "]") {
+                    bypass = false;
+                }
+                continue;
+            }
+
+
             if (LineBreakChars.beforeOnly.has(char)) {
                 !lastWasBreak && ret.push(br);
                 ret.push(char);
--- src/public/js/proof/proof.js
+++ src/public/js/proof/proof.js
@@ -22,7 +22,6 @@ const conf = {
   allowOverlap: false,
   showRules: true,
   showSubProofs: false,
-  showPopover: true,
   isMagic: false,
   isRuleShort: false,
   isLinear: false,
@@ -65,12 +64,6 @@ const conf = {
 
   load: function (path) {
     const file = path ? path : "../data/" + getSessionId() + "/" + getFileName();
-
-    const url = new URL(window.location.toLocaleString()).searchParams;
-    if (url.get("cond") === "sp") { // TODO remove this, and extra extract-examples in package.json
-      proof.showSubProofs = true;
-      proof.showPopover = false;
-    }
 
     if (file.endsWith(".json")) {
       d3.json(file).then(json => {
